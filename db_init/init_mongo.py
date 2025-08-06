4bcf23c6a6bcd9c2e8ffc8686815434c5cf4e879
pratham <hp@RelevantGuy.localdomain>
2025-08-06 19:48:06 +0530

    init_mongo.py updated

--- db_init/init_mongo.py
+++ db_init/init_mongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import json
 
-client = MongoClient("mongodb://root:example@localhost:27017/")
+client = MongoClient("mongodb://root:example@mongo:27017/?authSource=admin")  # ← updated
 db = client["hrmongo"]
 
 with open("db_init/personnel_info.json") as f:
