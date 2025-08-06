4a31a0b7a10413b1dc778d21aa6ee92f9c0270f5
pratham <kruikeypratham@gmail.com>
2025-08-06 10:33:01 +0530

    reset_db.sql added

---
+++ db_init/reset_db.sql
@@ -0,0 +1,12 @@
+DROP VIEW IF EXISTS 
+    experienced_employees, 
+    low_performers, 
+    top_earners_per_department, 
+    promotion_candidates 
+CASCADE;
+
+DROP TABLE IF EXISTS 
+    salary_log, 
+    professional_info, 
+    employee 
+CASCADE;
